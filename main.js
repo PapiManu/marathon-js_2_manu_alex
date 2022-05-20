150b806a221ffea0af804f7cf5ac0f59c7b5e166
AlexOnWax <alexandre.barbet@hotmail.com>
2022-05-20 16:51:43 +0200

    maj js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
 const hour =document.querySelector('#horloge > p')
 const hourAbbr =document.querySelector('#horloge > p > span')
+const quote =document.querySelector('#p_height_page')
+const authorQuote = document.querySelector('#span_height_page')
 
-console.log(hourAbbr);
+
 fetch("http://worldtimeapi.org/api/ip").then((response) =>
   response.json().then((data) => {
-    console.log(data);
     let unix_timestamp = (data.unixtime)
     // Create a new JavaScript Date object based on the timestamp
     // multiplied by 1000 so that the argument is in milliseconds, not seconds.
@@ -29,17 +30,32 @@ fetch("http://worldtimeapi.org/api/ip").then((response) =>
     const abbreviation = document.createTextNode(`${abbr}`);
 
 
-   hour.prepend(time)
-   hourAbbr.appendChild(abbreviation)
+  
+    hour.prepend(time)
+    hourAbbr.appendChild(abbreviation)
+  
 
 
 
   }))
 
+  
+
+  fetch("https://api.quotable.io/random").then((response) =>
+  response.json().then((dataQuote) => {
+    let quoteAuthor = dataQuote.author;
+  let quoteRandom = dataQuote.content;
+  const quoteRandomText = document.createTextNode(`${quoteRandom}`);
+  const quoteAuthorText = document.createTextNode(`${quoteAuthor}`);
+
+  quote.appendChild(quoteRandomText)
+
+console.log(dataQuote);
+
+  authorQuote.appendChild(quoteAuthorText)
 
 
-
-
+  }))
 
 
       
