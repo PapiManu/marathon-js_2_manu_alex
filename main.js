2883dee0e57548b1516b65f3cf66a30f117adccc
AlexOnWax <alexandre.barbet@hotmail.com>
2022-05-30 10:50:03 +0200

    maj hours

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const hourAbbr =document.querySelector('#horloge > p > span')
 const quote =document.querySelector('#p_height_page')
 const authorQuote = document.querySelector('#span_height_page')
 const city =document.querySelector('#position p')
+console.log(hourAbbr);
 
+
+function timeSet() {
 fetch("http://worldtimeapi.org/api/ip").then((response) =>
   response.json().then((data) => {
     let unix_timestamp = (data.unixtime)
@@ -16,14 +19,14 @@ fetch("http://worldtimeapi.org/api/ip").then((response) =>
     // Minutes part from the timestamp
     var minutes = "0" + date.getMinutes();
     // Seconds part from the timestamp
-    // var seconds = "0" + date.getSeconds();
+     var seconds = "0" + date.getSeconds();
     
     // Will display time in 10:30:23 format
     
 
 
-    // let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
-     let formattedTime = hours + ':' + minutes.substr(-2) ;
+    let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+     //let formattedTime = hours + ':' + minutes.substr(-2) ;
     
 
     
@@ -33,13 +36,22 @@ fetch("http://worldtimeapi.org/api/ip").then((response) =>
     
 
 
-    hour.prepend(time)
-    hourAbbr.appendChild(abbreviation)
+    hour.replaceChildren(time)
+    hourAbbr.replaceChildren(abbreviation)
+
+    
+
+
+
+  }))}
   
+ 
+  
+ setInterval(()=>{
+    timeSet()
+  }, 1000);
 
 
-
-  }))
   fetch("https://geolocation-db.com/json/").then((response) =>
     response.json().then((dataLocalisation) => {
       // let timeZone = data.timezone
