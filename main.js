c475face33bc298f98aa450c84c204ce8f49803b
AlexOnWax <alexandre.barbet@hotmail.com>
2022-05-31 16:13:20 +0200

    finalisation du hover du bouton More

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const hourAbbr = document.querySelector("#horloge > span");
 const quote = document.querySelector("#p_height_page");
 const authorQuote = document.querySelector("#span_height_page");
 const city = document.querySelector("#position p");
-
 const cont = document.querySelector("body");
 const curentTimezoneTitle = document.querySelector("#left_1");
 const curentTimezone = document.querySelector("#position_geographique");
@@ -19,13 +18,21 @@ const divTop = document.querySelector(".height_page");
 const buttonText = document.querySelector(".button-look > p > button");
 const buttonLogo = document.querySelector(".img-fleche");
 const info = document.querySelector(".info-grid");
-const refresh =document.querySelector('.button_refresh')
-const messageGently=document.querySelector('#messageGently')
-const logoGently=document.querySelector('#meteo_img > p > img')
+const refresh = document.querySelector(".button_refresh");
+const messageGently = document.querySelector("#messageGently");
+const logoGently = document.querySelector("#meteo_img > p > img");
+const buttonMore = document.querySelector("#button_more");
 
+//hover du bouton more_________________________________________
 
+buttonMore.addEventListener("mouseenter", () => {
+  buttonLogo.style.fill = "grey";
+});
+buttonMore.addEventListener("mouseout", () => {
+  buttonLogo.style.fill = "black";
+});
+//______________________________________________________________
 
-console.log(messageGently);
 i = 0;
 more.addEventListener("click", () => {
   i++;
@@ -46,7 +53,6 @@ more.addEventListener("click", () => {
 function timeSet() {
   fetch("http://worldtimeapi.org/api/ip").then((response) =>
     response.json().then((data) => {
-
       let unix_timestamp = data.unixtime;
       // Create a new JavaScript Date object based on the timestamp
       // multiplied by 1000 so that the argument is in milliseconds, not seconds.
@@ -61,12 +67,9 @@ function timeSet() {
       // Will display time in 10:30:23 format
       //let formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
       let formattedTime = hours + ":" + minutes.substr(-2);
-
       const continentContent = document.createTextNode(`${data.timezone}`);
-
       const time = document.createTextNode(`${formattedTime}`);
       const abbreviation = document.createTextNode(`${data.abbreviation}`);
-
       const curentTimezoneContent = document.createTextNode(`${data.timezone}`);
       const dayOfTheyearContent = document.createTextNode(
         `${data.day_of_year}`
@@ -75,36 +78,31 @@ function timeSet() {
         `${data.day_of_week}`
       );
       const weekNumberContent = document.createTextNode(`${data.week_number}`);
-
       hour.replaceChildren(time);
       hourAbbr.replaceChildren(abbreviation);
-
-      
       curentTimezone.replaceChildren(curentTimezoneContent);
       dayOfTheyear.replaceChildren(dayOfTheyearContent);
       dayOfTheWeek.replaceChildren(dayOfTheWeekContent);
       weekNumber.replaceChildren(weekNumberContent);
 
-      if(hours >= 05 && hours < 12 ) {
+      if (hours >= 05 && hours < 12) {
         console.log("it is the night");
-        messageGently.replaceChildren('GOOD MORNING, IT’S CURRENTLY')
-        logoGently.src="/assets/desktop/icon-moon.svg";
-        cont.style.backgroundImage="url(/assets/desktop/bg-image-daytime.jpg)"
-      }else if(hours >= 12 && hours < 17 ){
+        messageGently.replaceChildren("GOOD MORNING, IT’S CURRENTLY");
+        logoGently.src = "/assets/desktop/icon-moon.svg";
+        cont.style.backgroundImage =
+          "url(/assets/desktop/bg-image-daytime.jpg)";
+      } else if (hours >= 12 && hours < 17) {
         console.log("it is the day");
-        messageGently.replaceChildren('GOOD AFTERNOON, IT’S CURRENTLY')
-        logoGently.src="/assets/desktop/icon-sun.svg";
-        cont.style.backgroundImage="url(/assets/desktop/bg-image-daytime.jpg)"
-
-      }else {
-        messageGently.replaceChildren('GOOD EVENING, IT’S CURRENTLY')
-        logoGently.src="/assets/desktop/icon-moon.svg";
-        cont.style.backgroundImage="url(/assets/desktop/bg-image-nighttime.jpg)"
+        messageGently.replaceChildren("GOOD AFTERNOON, IT’S CURRENTLY");
+        logoGently.src = "/assets/desktop/icon-sun.svg";
+        cont.style.backgroundImage =
+          "url(/assets/desktop/bg-image-daytime.jpg)";
+      } else {
+        messageGently.replaceChildren("GOOD EVENING, IT’S CURRENTLY");
+        logoGently.src = "/assets/desktop/icon-moon.svg";
+        cont.style.backgroundImage =
+          "url(/assets/desktop/bg-image-nighttime.jpg)";
       }
-
-      // "Good morning" between 5am and 12pm
-      // - "Good afternoon" between 12pm and 6pm
-      // - "Good evening" between 6pm and 5am
     })
   );
 }
@@ -130,25 +128,20 @@ fetch("https://geolocation-db.com/json/").then((response) =>
 
 //Api Citation_____________________________________________________
 function refreshButton() {
-fetch("https://api.quotable.io/random").then((response) =>
-  response.json().then((dataQuote) => {
-    let quoteAuthor = dataQuote.author;
-    let quoteRandom = dataQuote.content;
-    const quoteRandomText = document.createTextNode(`${quoteRandom}`);
-    const quoteAuthorText = document.createTextNode(`${quoteAuthor}`);
-    quote.replaceChildren(quoteRandomText);
-    authorQuote.replaceChildren(quoteAuthorText);
-  })
-);
+  fetch("https://api.quotable.io/random").then((response) =>
+    response.json().then((dataQuote) => {
+      let quoteAuthor = dataQuote.author;
+      let quoteRandom = dataQuote.content;
+      const quoteRandomText = document.createTextNode(`${quoteRandom}`);
+      const quoteAuthorText = document.createTextNode(`${quoteAuthor}`);
+      quote.replaceChildren(quoteRandomText);
+      authorQuote.replaceChildren(quoteAuthorText);
+    })
+  );
 }
-
- refreshButton()
-
- refresh.addEventListener('click', () => {
- 
-  
-   refreshButton();
- })
- 
+refreshButton();
+refresh.addEventListener("click", () => {
+  refreshButton();
+});
 
 //_____________________________________________________________________
